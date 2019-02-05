18d256e01f6954f09f8e541b2ee78dfcb521c97c
brian604 <kjwk1221@gmail.com>
2019-02-04 19:50:39 -0800

    here is a R code for basics and mathematical application

---
+++ infobasic.R
@@ -0,0 +1,23 @@
+### Statistical Computing ### ----
+
+### Combinations and Permutations in R ----
+## Author : Jiwon Kim 
+# Probability 
+
+# Initiatlize the empty vector 
+n <- c()
+# How would I initiatize the empty data-frame?
+
+for (i in 1:3){
+  val <- c(8:10)
+  n[i] <- choose(10,val[i])
+}
+
+expand_bn <- function(a,b,c){
+  if (c = 1){  # c is power 
+    return (paste(as.character(1),paste("-",b)))}
+  else{
+    
+    b_p <- paste0(b,p)
+  }
+}
