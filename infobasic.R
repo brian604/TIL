62f610f00bbc6dbb9a5509444fb6f800b3fcb252
brian604 <kjwk1221@gmail.com>
2019-02-12 22:10:59 -0800

    some computing and stuff

--- infobasic.R
+++ infobasic.R
@@ -3,6 +3,19 @@
 ### Combinations and Permutations in R ----
 ## Author : Jiwon Kim 
 # Probability 
+p <- c(0.8, 0.6,0.2)
+weather <- c(0.1,0.6,0.3)
+p_vec <- c()
+for (i in 1:3){
+  p_vec[i] <- weather[i] * choose(10,4)*p[i]^4*(1-p[i])^(i)^6
+}
+
+# sigma of the series 
+l <- c(0.16,0.84)
+p_vec1 <- c()
+for (i in 1:5){
+  p_vec[i] <- (l[1]^i)*(l[2]^(5-i))
+}
 
 # Initiatlize the empty vector 
 n <- c()
@@ -21,3 +34,10 @@ expand_bn <- function(a,b,c){
     b_p <- paste0(b,p)
   }
 }
+
+### Probability distribution in R ----
+# NegBin = "finding # of trials on which rth success occur" 
+
+
+
+
